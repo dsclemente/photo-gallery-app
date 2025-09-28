a6b732b50891e78eb0d507c3cb5ffcb0c20f556b
Daniel Clemente <daniel.clemente@sortfy.com.br>
2025-09-28 15:11:06 -0300

    update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'photo-gallery',
-  webDir: 'www'
+  webDir: 'www',   plugins: {
+    LiveUpdates: {
+      appId: '1d6ee7e7',
+      channel: 'Production',
+      autoUpdateMethod: 'background',
+      maxVersions: 2
+    }
+  }
 };
 
 export default config;
